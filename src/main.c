bba7ab49dd13c8c195bd858f97a94643e3ab5e42
ozneroL541 <l.21radice@gmail.com>
2025-10-05 01:32:46 +0200

    Update copyright notice and licensing information in main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,21 @@
+/*   
+ *    Spiral Matrix
+ *    Copyright (C) 2025  Lorenzo Radice
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <stdlib.h>
 #include <math.h>
 #define RIGHT 0
@@ -177,7 +195,10 @@ void generator(const unsigned long n) {
     /* Delete matrix */
     deletem(sol, n);
 }
-
+/*
+ * Spiral Matrix
+ * @author Lorenzo Radice
+*/
 int main(int argc, char const *argv[]) {
     unsigned long input = 0;
     if (argc < 1) {
